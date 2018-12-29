25e7b47e932acbbe2f1b0552e7c2e82315a55d9f
Davey Winter <rdwinter3@gmail.com>
2018-12-29 09:19:11 -0600

    first commit

---
+++ src/com/example/EulerSolution.java
@@ -0,0 +1,9 @@
+package com.example;
+
+// This forces every solution class to implement a common interface,
+// which is helpful for unit testing like in the EulerTest implementation.
+public interface EulerSolution {
+
+    public String run();
+
+}
